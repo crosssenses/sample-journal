da373cf81ab934812bcd866628b4e9132e6efa91
mhebing <marcel@ddionrails.org>
2018-02-19 17:33:05 +0100

    Update local settings to point to the project directory

--- agile-de/local_settings.py
+++ agile-de/local_settings.py
@@ -2,7 +2,18 @@ import os, sys
 sys.path.append(os.path.abspath("../../beautiful-reports/django"))
 from settings.base import *
 
+# Development Settings
+
 DEBUG=True
 SUPER_DEBUG=False
-import sys, os
 
+# Path settings
+
+PROJECT_DIR = os.path.dirname(__file__)
+PAGES_DIRECTORY = PROJECT_DIR
+OUTPUT_DIRECTORY=os.path.join(BASE_DIR, "_build")
+
+STATICFILES_DIRS = [
+    STATIC_ROOT,
+    os.path.join(PROJECT_DIR, "static"),
+]
