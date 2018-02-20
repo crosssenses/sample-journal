6a34b558f6b523c9a9d65c44cf4f75ed247aa425
mhebing <marcel@ddionrails.org>
2018-02-20 13:07:44 +0100

    Include styles

--- scripts/agile_dev.sh
+++ scripts/agile_dev.sh
@@ -14,10 +14,12 @@ then
   
   tmux new-window -t beautiful -n work
   tmux split-window -h -t beautiful:1
-  # tmux split-window -v -t beautiful:1.0
-  # tmux split-window -v -t beautiful:1.1
+  tmux split-window -v -t beautiful:1.0
+  tmux split-window -v -t beautiful:1.1
   tmux send-keys -t beautiful:1.0 "cd agile-de; python manage.py runserver 8001" C-m
   tmux send-keys -t beautiful:1.1 "cd agile-en; python manage.py runserver 8002" C-m
+  tmux send-keys -t beautiful:1.2 "sass --watch styles/horst/horst.scss:agile-de/static/css/bootstrap-horst.css" C-m
+  tmux send-keys -t beautiful:1.3 "sass --watch styles/horst/horst.scss:agile-en/static/css/bootstrap-horst.css" C-m
   # tmux send-keys -t beautiful:1.1 "paver elastic" C-m
   # tmux send-keys -t beautiful:1.3 "paver rqworker" C-m
 fi
