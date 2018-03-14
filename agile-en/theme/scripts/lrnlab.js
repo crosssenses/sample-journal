b8af4f98c762367f129b18db2dc3835ff66ad054
mhebing <marcel@ddionrails.org>
2018-03-14 13:47:34 +0100

    Add local js file (IE!) #67

---
+++ agile-en/theme/scripts/lrnlab.js
@@ -0,0 +1,6 @@
+var isIE = /*@cc_on!@*/false || !!document.documentMode;
+$(function(){if (isIE){
+    $(".ms-header .card .card-body").css("margin-top", "100px !important");
+    $(".ms-chapter .ms-row-full .ms-col-content").css("margin-top", "400px !important");
+}})
+console.log("Loading theme JS")
