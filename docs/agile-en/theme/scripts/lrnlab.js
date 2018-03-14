ea2dd7916a42bfee4af3d61912f9d70842acc219
mhebing <marcel@ddionrails.org>
2018-03-14 14:16:02 +0100

    IE fix: 600px

--- docs/agile-en/theme/scripts/lrnlab.js
+++ docs/agile-en/theme/scripts/lrnlab.js
@@ -3,7 +3,7 @@ var isIE = /*@cc_on!@*/false || !!document.documentMode;
 $(function(){if (isIE){
     console.log("Detected: IE");
     $(".ms-header .card .card-body").css("cssText", "margin-top: 100px !important;");
-    $(".ms-chapter .ms-row-full .ms-col-content").css("cssText", "margin-top: 400px !important;");
+    $(".ms-chapter .ms-row-full .ms-col-content").css("cssText", "margin-top: 600px !important;");
 }else{
     console.log("Detected: not IE");
 }})
