e5042f27db9328b16f14cd5840507100f101e43b
crosssenses <lari_wu@gmx.de>
2020-11-12 15:17:21 +0100

    cleanup

--- docs/agile-de/theme/scripts/lrnlab.js
+++
@@ -1,9 +0,0 @@
-console.log("Loading theme JS")
-var isIE = /*@cc_on!@*/false || !!document.documentMode;
-$(function(){if (isIE){
-    console.log("Detected: IE");
-    $(".ms-header .card .card-body").css("cssText", "margin-top: 100px !important;");
-    $(".ms-chapter .ms-row-full .ms-col-content").css("cssText", "margin-top: 600px !important;");
-}else{
-    console.log("Detected: not IE");
-}})
